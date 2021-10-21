029d7b77186c3cab2b34ce9fc5b17e02f4f0e4ca
soham6125 <chaudhuri.ber@gmail.com>
2021-10-21 12:05:45 +0530

    UPDATE

---
+++ Problem009.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define ll               long long int 
+#define loop(a,b,i)      for(long long int i=a;i<b;i++)
+#define loopr(a,b,i)     for(long long int i=a;i>=b;i--)
+#define cn               continue;
+#define pb               push_back
+#define db               double
+#define mp               make_pair
+#define sz(x)            (ll)((x).size())
+#define endl             "\n"
+#define lb               lower_bound
+#define ub               upper_bound
+#define f                first
+#define se               second
+#define vll              vector<ll>
+#define pll              pair<ll,ll>
+#define vpll             vector< pair<ll,ll> >
+#define all(x)           x.begin(),x.end()
+#define pi               3.14159265358979323846
+#define quick            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const ll mod = 1e9 + 7;
+const ll MAX  = 8000000000000000064LL;
+const ll MIN  = -8000000000000000064LL;
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+ll gcd(ll a,ll b) {if(b==0)return a; return gcd(b,a%b);}
+ll lcm(ll x, ll y) { ll res = x / gcd(x, y); return (res * y);}
+// ll nCr(ll n,ll r){ll ans=fact[n]; ans*=mod_inv(fact[n-r]);ans%=mod; ans*=mod_inv(fact[r]);ans%=mod; return ans;}
+using namespace std; 
+
+const ll dx[] = {-1,1,0,0,1,1,-1,-1};
+const ll dy[] = {0,0,1,-1,-1,1,-1,1};
+
+signed main() 
+{ 
+    long long product, temp;
+    int a, b, c, n, t; 
+    cin >> t; 
+    for(int i = 0; i < t; i++)
+    {
+        cin >> n;
+        product = -1; 
+        for(int a = 1, b, c; a <= n/3; a++)
+        {
+            b = (n*n - 2*n*a)/(2*n - 2*a);
+            c = n - a - b; 
+            if(c*c == (a*a + b*b))
+            {
+                temp = a*b*c; 
+                if(temp > product)
+                {
+                    product = temp; 
+                }
+            }
+        }
+        cout << product << endl;
+    }
+
+cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+return 0;
+}
